fd1a7563e63599a2f67eda0ffae046c901cf44e9
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-03-11 01:48:34 +0000

    [Finish] フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,10 +15,24 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :new
+    end
+  end
+
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      render :new
+    end
   end
 
   def destroy
